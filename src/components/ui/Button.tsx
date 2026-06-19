290e6f911f75abf83cf03dbc301958773f53d7ce
ahmed <ahmedmijdo2@gmail.com>
2026-06-19 23:46:39 +0300

    button component base variants

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,10 +1,23 @@
 import { ButtonHTMLAttributes, ReactNode } from "react";
+import { cva } from "class-variance-authority";
 
 interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode;
     className?: string;
     width?: "w-full" | "w-fit";
 }
+
+const buttonVariants = cva("rounded-lg font-semibold", {
+    variants: {
+        intent: {
+            primary: "bg-black text-white dark:bg-white dark:text-black",
+            outline: "bg-transparent text-black border border-gray-300 dark:text-white"
+        }
+    },
+    defaultVariants: {
+        intent: "primary",
+    }
+});
 
 const Button = ({children, className, width = "w-full", ...rest}: IProps) => {
     return (
